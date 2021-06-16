2701d502d19ee72f97647e4845c9f24d79f2a956
Sarah <sarahkiehs@gmail.com>
2021-06-17 01:10:15 +0800

    late load dont

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link rel='prefetch' href='https://fonts.gstatic.com'/>
           <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap' rel='stylesheet' />
         </Head>
         <body>
