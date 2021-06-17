041c0dd75b1bdaaf458b4556c7a68bc6b761d93b
Sarah <sarahkiehs@gmail.com>
2021-06-17 12:20:12 +0800

    uo

--- components/AboutMe/index.tsx
+++ components/AboutMe/index.tsx
@@ -2,15 +2,12 @@ import { pageRoutes } from "../../lib/routes";
 
 const AboutMe = () => (
   <div className="flex flex-col space-y-4 text-md md:max-w-2xl place-self-center">
-    <h1>My StOrY</h1>
+    <h1>My Story</h1>
     <p>I am Falak.</p>
-    <p>Yo yo.</p>
     <h2>
       <a href={pageRoutes.writing}>Writing</a>
     </h2>
     I have written some stuff here
-    <h2>Shop [TODO]</h2>
-    Support me by buying from here hdsuwdcouhewocevcecvechberiuycbeuricbeuicbeuicfhuifchenufheuheuichreunbfrewnbfuiorfhoirufhin3ofhr3ohnchiewchnichenwichenw
   </div>
 );
 
